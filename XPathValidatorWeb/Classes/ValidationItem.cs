020f5ceb18a77b60356a0e97368ce87736a6aa26
Tom Austin <Tom.Austin@streets-heaver.com>
2020-04-03 16:31:34 +0100

    Add project files.

---
+++ XPathValidatorWeb/Classes/ValidationItem.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XPathValidatorWeb.Classes
+{
+    public class ValidationItem : IValidatableObject
+    {
+
+        [BindProperty]
+        [Required]
+        [Display(Name = "XPath Expression")]
+        public string XPathExpression { get; set; }
+
+        [BindProperty]
+        [Display(Name = "XML")]
+        public string XML { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+            try
+            {
+                System.Xml.XPath.XPathExpression.Compile(XPathExpression);
+            }
+            catch (Exception ex)
+            {
+                results.Add(new ValidationResult(ex.Message));
+            }
+
+
+            //if (XPathExpression.)
+            //{
+            //    yield return new ValidationResult(
+            //        $"Classic movies must have a release year no later than {_classicYear}.",
+            //        new[] { nameof(ReleaseDate) });
+            //}
+
+            return results;
+        }
+    }
+}
---
+++ XPathValidatorWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using XPathValidatorWeb.Classes;
+
+namespace XPathValidatorWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public ValidationItem Main { get; set; }
+
+       
+
+      
+
+        public ActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return Page();
+        }
+    }
+}
